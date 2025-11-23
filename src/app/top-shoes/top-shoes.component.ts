c7e25fe7e78b70c38ff04201ec1214d09b7e3dd2
IvanRuizNavarro47 <145495051+IvanRuizNavarro47@users.noreply.github.com>
2025-11-23 21:29:23 +0100

    Banner shoes news

--- src/app/top-shoes/top-shoes.component.ts
+++ src/app/top-shoes/top-shoes.component.ts
@@ -8,6 +8,8 @@ import { CommonModule } from '@angular/common';
   templateUrl: './top-shoes.component.html',
   styleUrls: ['./top-shoes.component.scss']
 })
+
+
 export class TopShoesComponent {
   topShoes = [
     { name: "Nike Book 1 Sedona", player: "Devin Booker", year: 1985, photo: "assets/book.jpeg" },
@@ -15,4 +17,25 @@ export class TopShoesComponent {
     { name: "Adidas Harden Vol.4", player: "James Harden", year: 2019, photo: "assets/harden9.jpg" },
     { name: "Anthony Edwards 2", player: "Anthony Edwards", year: 2020, photo: "assets/ant2.jpg" }
   ];
+
+  currentIndex = 0;
+
+next() {
+  this.currentIndex = (this.currentIndex + 1) % 3;
 }
+
+prev() {
+  this.currentIndex = (this.currentIndex - 1 + 3) % 3;
+}
+
+goTo(i: number) {
+  this.currentIndex = i;
+}
+
+ngOnInit() {
+  setInterval(() => this.next(), 4000);
+}
+
+}
+
+
